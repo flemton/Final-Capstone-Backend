73970e4c0c6658c3c129c66e5d8e996ce4ed8f5e
Alhassan Osman <oalhassan847@gmail.com>
2023-12-07 11:01:11 +0000

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    get 'users/current', to: 'users#current'
+
+    resources :tesla_models do
+      member do
+        get 'image'
+      end
+      resources :reservations, only: [:create, :index]
+    end
+
+    resources :users, only: [:index, :create, :show]
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
+
+    resources :reservations, only: [:show, :update, :destroy]
+    get 'user_reservations', to: 'reservations#user_reservations'
+  end
 end
