2e59c2fc35d925889a8bb53f0da986d3ba73fb36
Alhassan Osman <oalhassan847@gmail.com>
2023-12-07 01:35:42 +0000

    Run migrations

--- db/migrate/20231207010824_create_users.rb
+++ db/migrate/20231207010824_create_users.rb
@@ -2,7 +2,6 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :username
-      t.string :encrypted_password
 
       t.timestamps
     end
