4fcd76692f00a9c2565c1854f8fbfca827f25007
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-13 17:08:12 -0500

    Generate reservations_controller.rb w/ basic CRUD methods

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -1,2 +1,11 @@
-class ReservationsController < ApplicationController
+class Api::ReservationsController < ApplicationController
+  def index; end
+
+  def show; end
+
+  def create; end
+
+  def update; end
+
+  def destroy; end
 end
