8cb6fc343dd36064c39f594402d53cbcf4d66bbf
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-13 17:10:33 -0500

    Add private methods and actions before running reservations_controller.rb

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -1,11 +1,28 @@
 class Api::ReservationsController < ApplicationController
-  def index; end
+  before_action :set_tesla_model, only: [:create]
+  before_action :set_reservation, only: %i[show update destroy]
+  def index
+    @reservations = current_user.reservations
+    render json: @reservations
+  end
 
-  def show; end
+  def show
+    render json: @reservation
+  end
 
   def create; end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def set_tesla_model
+    @tesla_model = TeslaModel.find(params[:tesla_model_id])
+  end
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
 end
