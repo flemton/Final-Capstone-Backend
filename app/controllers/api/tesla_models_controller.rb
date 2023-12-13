4884dcd14fb3adbf6cbdf2d9d0e27d378a5ba49f
SFarida <faridagueleu@gmail.com>
2023-12-13 23:54:34 +0100

    Add index method to load data

--- app/controllers/api/tesla_models_controller.rb
+++ app/controllers/api/tesla_models_controller.rb
@@ -1,2 +1,12 @@
-class TeslaModelsController < ApplicationController
+class Api::TeslaModelsController < ApplicationController
+  skip_before_action :set_current_user, only: [:create]
+  def index
+    @tesla_models = TeslaModel.all
+    tesla_models_json = @tesla_models.map do |tesla_model|
+      tesla_model_attributes = tesla_model.attributes
+      tesla_model_attributes['image_url'] = url_for(tesla_model.image) if tesla_model.image.attached?
+      tesla_model_attributes
+    end
+    render json: tesla_models_json
+  end
 end
