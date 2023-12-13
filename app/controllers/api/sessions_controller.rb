a6314c724ed647cfe651a6cf87bfe16a38e0cf6d
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-13 17:16:56 -0500

    Add sessions_controller.rb w/ create method & strong params

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,17 @@
-class SessionsController < ApplicationController
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      render json: { success: true, user_id: @user.id, message: 'Sign in was successful!' }
+    else
+      render json: { error: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
