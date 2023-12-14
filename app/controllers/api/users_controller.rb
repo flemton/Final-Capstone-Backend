9cd4188969adf16dfe02a33994055d475513147e
SFarida <faridagueleu@gmail.com>
2023-12-14 04:10:20 +0100

    Create new user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,14 @@
 class Api::UsersController < ApplicationController
   before_action :set_current_user, only: [:current]
-
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      render json: { success: true, user_id: @user.id, message: 'Sign in was successful!' }, status: :created
+    else
+      render json: { error: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
   def current
     if @current_user
       render json: @current_user
@@ -8,7 +16,7 @@ class Api::UsersController < ApplicationController
       render json: { error: 'Not logged in' }, status: :unauthorized
     end
   end
-  
+
   private
 
   def set_current_user
