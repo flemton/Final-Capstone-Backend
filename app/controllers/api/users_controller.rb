50aa5796ef849153b3b75e76d596be546cc1afee
SFarida <faridagueleu@gmail.com>
2023-12-14 04:11:29 +0100

    Add show user method

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,9 @@
 class Api::UsersController < ApplicationController
   before_action :set_current_user, only: [:current]
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
   def create
     @user = User.new(user_params)
     if @user.save
