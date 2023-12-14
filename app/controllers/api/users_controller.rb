08f90e7fc72ada9a8e30df70098351c62c73e706
SFarida <faridagueleu@gmail.com>
2023-12-14 04:13:05 +0100

    Add index method in user controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,9 @@
 class Api::UsersController < ApplicationController
   before_action :set_current_user, only: [:current]
+  def index
+    @users = User.all
+    render json: @users
+  end
   def show
     @user = User.find(params[:id])
     render json: @user
