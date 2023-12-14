056f2f0c1b3ed8c936615c0e4885135cf0f7edd8
Alhassan Osman <oalhassan847@gmail.com>
2023-12-14 08:23:53 +0000

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :reservations
+
+  validates :username, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, allow_blank: false
 end
