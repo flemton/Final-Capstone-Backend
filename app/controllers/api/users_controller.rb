8725215c66d455072ed0afacff1df3ae5ea831d5
SFarida <faridagueleu@gmail.com>
2023-12-14 04:09:13 +0100

    Add method to check if user is logged in

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,14 @@
 class Api::UsersController < ApplicationController
   before_action :set_current_user, only: [:current]
 
+  def current
+    if @current_user
+      render json: @current_user
+    else
+      render json: { error: 'Not logged in' }, status: :unauthorized
+    end
+  end
+  
   private
 
   def set_current_user
