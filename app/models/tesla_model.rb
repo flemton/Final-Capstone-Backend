a3fd8ba752c1a0ddd58f211f59f4cbefdcfb8c75
Alhassan Osman <oalhassan847@gmail.com>
2023-12-15 20:59:27 +0000

    Add image to tesla model and validations

--- app/models/tesla_model.rb
+++ app/models/tesla_model.rb
@@ -1,3 +1,13 @@
 class TeslaModel < ApplicationRecord
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
+
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :deposit, numericality: { greater_than_or_equal_to: 0 }
+  validates :finance_fee, numericality: { greater_than_or_equal_to: 0 }
+  validates :option_to_purchase_fee, numericality: { greater_than_or_equal_to: 0 }
+  validates :total_amount_payable, numericality: { greater_than_or_equal_to: 0 }
+  validates :duration, numericality: { greater_than_or_equal_to: 0 }
 end
