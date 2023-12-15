7c06c82186afa51339e98432216482fd3a7e4420
SFarida <faridagueleu@gmail.com>
2023-12-15 02:37:01 +0100

    Fix linters

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -3,7 +3,7 @@ class Api::SessionsController < ApplicationController
     @user = User.find_by(email: params[:user][:email])
 
     if @user&.valid_password?(params[:user][:password])
-       sign_in(@user)
+      sign_in(@user)
       render json: { success: true, user_id: @user.id, message: 'Sign in was successful!' }
     else
       render json: { error: 'Invalid email or password' }, status: :unauthorized
