3e7bc50a37a5de32adcb4821ff157243e15a0a65
Alhassan Osman <oalhassan847@gmail.com>
2023-12-17 15:58:19 +0000

    Remove email and password from session(login)

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,18 +1,18 @@
 class Api::SessionsController < ApplicationController
   def create
-    @user = User.find_by(email: params[:user][:email])
+    @user = User.find_by(username: params[:user][:username])
 
-    if @user&.valid_password?(params[:user][:password])
+    if @user
       session[:user_id] = @user.id
       render json: { success: true, user_id: @user.id, message: 'Sign in was successful!' }
     else
-      render json: { error: 'Invalid email or password' }, status: :unauthorized
+      render json: { error: 'Invalid username' }, status: :unauthorized
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:username)
   end
 end
