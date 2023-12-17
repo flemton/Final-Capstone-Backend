a9738c89b6aad967d73d67fd9d8f7763a825962c
Alhassan Osman <oalhassan847@gmail.com>
2023-12-17 00:35:02 +0000

    Modified controllers and config to save current user to session

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,14 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :tesla_model
+
+  def reservation_with_tesla_model
+    {
+      id:,
+      start_date:,
+      end_date:,
+      city:,
+      tesla_model: tesla_model.attributes
+    }
+  end
 end
