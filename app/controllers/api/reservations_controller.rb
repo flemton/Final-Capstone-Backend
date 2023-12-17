c7a972f1ec9a868b352d421bcb280597508f3751
Alhassan Osman <oalhassan847@gmail.com>
2023-12-17 00:35:10 +0000

    Update reservations_controller.rb

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -3,13 +3,13 @@ class Api::ReservationsController < ApplicationController
   before_action :set_reservation, only: %i[show update destroy]
 
   def user_reservations
-    @reservations = current_user.reservations
+    @reservations = @current_user.reservations
     response = @reservations.map(&:reservation_with_tesla_model)
     render json: response
   end
 
   def index
-    @reservations = current_user.reservations
+    @reservations = @current_user.reservations
     render json: @reservations
   end
 
@@ -19,17 +19,20 @@ class Api::ReservationsController < ApplicationController
 
   def create
     @reservation = Reservation.new(reservation_params)
-    @reservation.user = current_user
+    @reservation.user = User.find(params[:user_id])
+    puts 'User: ', @reservation.user
     @reservation.tesla_model = @tesla_model
 
-    if @tesla_model.available?(@reservation.start_time, @reservation.end_time)
+    if @tesla_model.available?(@reservation.start_date, @reservation.end_date)
       if @reservation.save
         render json: { success: true, reservation_id: @reservation.id }
       else
-        render json: { error: @reservation.errors.full_messages }, status: :unprocessable_entity
+        render json: { error: 'Error saving reservation', details: @reservation.errors.full_messages },
+               status: :unprocessable_entity
       end
     else
-      render json: { error: 'TeslaModel is not available for reservation' }, status: :unprocessable_entity
+      render json: { error: 'TeslaModel is not available for reservation', details: @tesla_model.errors.full_messages },
+             status: :unprocessable_entity
     end
   end
 
@@ -37,7 +40,8 @@ class Api::ReservationsController < ApplicationController
     if @reservation.update(reservation_params)
       render json: { success: true, reservation: @reservation }
     else
-      render json: { error: @reservation.errors.full_messages }, status: :unprocessable_entity
+      render json: { error: 'TeslaModel is not available for reservation', details: @tesla_model.errors.full_messages },
+             status: :unprocessable_entity
     end
   end
 
@@ -50,10 +54,13 @@ class Api::ReservationsController < ApplicationController
 
   def set_tesla_model
     @tesla_model = TeslaModel.find(params[:tesla_model_id])
+    return if @tesla_model
+
+    logger.error("TeslaModel with id #{params[:tesla_model_id]} not found")
   end
 
   def reservation_params
-    params.require(:reservation).permit(:start_time, :end_time, :city)
+    params.require(:reservation).permit(:user_id, :start_date, :end_date, :city)
   end
 
   def set_reservation
