e0c3079506223a1041f46471bc9cd7b648f48c26
Alhassan Osman <oalhassan847@gmail.com>
2023-12-17 15:59:08 +0000

    Make username unique to store users

--- db/migrate/20231207010824_create_users.rb
+++ db/migrate/20231207010824_create_users.rb
@@ -1,9 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :username
+      t.string :username, null: false
 
       t.timestamps
     end
+
+    add_index :users, :username, unique: true
   end
 end
